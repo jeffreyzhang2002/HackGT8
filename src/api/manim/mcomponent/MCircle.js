0775fc0b6f20436a876f6856b6e2aa5368a77c1b
Liusef <liujoseph2920@Outlook.com>
2021-10-23 04:37:10 -0400

    Some Manim Components 💀

---
+++ src/api/manim/mcomponent/MCircle.js
@@ -0,0 +1,12 @@
+import MComponent from "./MComponent";
+
+
+export default class MCircle extends MComponent{
+    constructor() {
+        super();
+        this.template = {
+            ...this.template,
+            Radius: undefined
+        }
+    }
+}
---
+++ src/api/manim/mcomponent/MPoint.js
@@ -0,0 +1,11 @@
+import MComponent from "./MComponent";
+
+export default class MPoint extends MComponent {
+    constructor() {
+        super();
+        this.template = {
+            ...this.template,
+            Radius: undefined
+        }
+    }
+}
